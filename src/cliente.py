7563a208dc44f2b4c21ed19f09adc75f60e4959f
Juliana101a <JulianaOseias.19939@ae-danielsampaio.pt>
2026-05-11 22:04:26 +0100

    Update cliente.py

--- src/cliente.py
+++ src/cliente.py
@@ -1,68 +1,61 @@
 
 from persistencia import guardar_dados
-from utils import validar_data
 
 clientes = {}
 contador = 1
 
 
-def criar_cliente(nome, nif, telefone, email, data_nascimento):
-
+def gerar_id():
     global contador
-
-    if not validar_data(data_nascimento):
-        return 400, "Data inválida"
-
-    id_cliente = f"C{contador:03d}"
+    cid = f"C{contador:03d}"
     contador += 1
+    return cid
 
-    clientes[id_cliente] = {
+
+def criar_cliente(nome, nif, telefone, email, data):
+    cid = gerar_id()
+    clientes[cid] = {
         "nome": nome,
         "nif": nif,
         "telefone": telefone,
         "email": email,
-        "data_nascimento": data_nascimento
+        "data": data
     }
-
     guardar_dados("clientes.json", clientes)
-
-    return 201, id_cliente
+    return 201, cid
 
 
 def listar_clientes():
-    return 200, [{"id_cliente": i, **d} for i, d in clientes.items()]
+    return 200, [{"id": k, **v} for k, v in clientes.items()]
 
 
-def consultar_cliente(id_cliente):
-    if id_cliente not in clientes:
-        return 404, "Não encontrado"
+def consultar_cliente(cid):
+    if cid not in clientes:
+        return 404, "não encontrado"
+    return 200, {"id": cid, **clientes[cid]}
 
-    return 200, {"id_cliente": id_cliente, **clientes[id_cliente]}
 
+def atualizar_cliente(cid, nome=None, nif=None, telefone=None, email=None, data=None):
+    if cid not in clientes:
+        return 404, "não encontrado"
 
-def atualizar_cliente(id_cliente, nome=None, nif=None, telefone=None, email=None, data_nascimento=None):
-
-    if id_cliente not in clientes:
-        return 404, "Não encontrado"
-
-    if nome is not None: clientes[id_cliente]["nome"] = nome
-    if nif is not None: clientes[id_cliente]["nif"] = nif
-    if telefone is not None: clientes[id_cliente]["telefone"] = telefone
-    if email is not None: clientes[id_cliente]["email"] = email
-    if data_nascimento is not None: clientes[id_cliente]["data_nascimento"] = data_nascimento
+    if nome: clientes[cid]["nome"] = nome
+    if nif: clientes[cid]["nif"] = nif
+    if telefone: clientes[cid]["telefone"] = telefone
+    if email: clientes[cid]["email"] = email
+    if data: clientes[cid]["data"] = data
 
     guardar_dados("clientes.json", clientes)
+    return 200, cid
 
-    return 200, id_cliente
 
+def remover_cliente(cid):
+    if cid not in clientes:
+        return 404, "não encontrado"
 
-def remover_cliente(id_cliente):
-
-    if id_cliente not in clientes:
-        return 404, "Não encontrado"
-
-    return 200, clientes.pop(id_cliente)
-
+    r = clientes.pop(cid)
+    guardar_dados("clientes.json", clientes)
+    return 200, r
 
 
 
