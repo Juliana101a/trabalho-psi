426c344cf558c361640bb457e55dc9faee8e5908
Juliana101a <JulianaOseias.19939@ae-danielsampaio.pt>
2026-05-11 22:14:18 +0100

    Refactor file parameter names in data functions

--- persistencia.py
+++ persistencia.py
@@ -2,16 +2,16 @@ import json
 import os
 
 
-def guardar_dados(nome_ficheiro, dados):
-    with open(nome_ficheiro, "w", encoding="utf-8") as f:
+def guardar_dados(ficheiro, dados):
+    with open(ficheiro, "w", encoding="utf-8") as f:
         json.dump(dados, f, indent=4, ensure_ascii=False)
 
 
-def carregar_dados(nome_ficheiro):
-    if not os.path.exists(nome_ficheiro):
+def carregar_dados(ficheiro):
+    if not os.path.exists(ficheiro):
         return {}
 
-    with open(nome_ficheiro, "r", encoding="utf-8") as f:
-        return json.load(f)
-
+    with open(ficheiro, "r", encoding="utf-8") as f:
+        conteudo = f.read().strip()
+        return json.loads(conteudo) if conteudo else {}
 
