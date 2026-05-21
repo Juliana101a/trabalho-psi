c9823598d2554806cb886031a7020d44199fb994
Juliana101a <JulianaOseias.19939@ae-danielsampaio.pt>
2026-05-21 12:15:17 +0100

    Update hotel.py

--- src/hotel.py
+++ src/hotel.py
@@ -1,4 +1,41 @@
 
+from persistencia import guardar_dados, carregar_dados
+from utils import gerar_id
+from logger import log_info, log_warning, log_error
+
+
+def criar_hotel(nome, endereco, telefone, classificacao):
+    hoteis = carregar_dados("hoteis.json")
+    hid = gerar_id("H", hoteis)
+
+    hoteis[hid] = {
+        "nome": nome,
+        "endereco": endereco,
+        "telefone": telefone,
+        "classificacao": classificacao
+    }
+
+    guardar_dados("hoteis.json", hoteis)
+    log_info(f"Hotel criado: {hid}")
+    return 201, hid
+
+
+def listar_hoteis():
+    hoteis = carregar_dados("hoteis.json")
+    log_info("Listagem hotéis")
+    return 200, [{"id": k, **v} for k, v in hoteis.items()]
+
+
+def consultar_hotel(hid):
+    hoteis = carregar_dados("hoteis.json")
+
+    if hid not in hoteis:
+        log_warning(f"Hotel não encontrado: {hid}")
+        return 404, "não encontrado"
+
+    log_info(f"Hotel consultado: {hid}")
+    return 200, {"id": hid, **hoteis[hid]}
+
 
 def atualizar_hotel(hid, nome=None, endereco=None, telefone=None, classificacao=None):
     hoteis = carregar_dados("hoteis.json")
@@ -29,8 +66,3 @@ def remover_hotel(hid):
 
     log_info(f"Hotel removido: {hid}")
     return 200, r
-
-
-
-
-
